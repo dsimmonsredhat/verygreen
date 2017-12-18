5ac81eccd25b3d4eb41f68d61082800c57971825
dsimmonsredhat <dsimmons@redhat.com>
2017-12-18 14:14:42 -0500

    asfd

--- hello.php
+++ hello.php
@@ -4,7 +4,7 @@
  </head>
  <body>
  <?php echo '<p>Hello I am Feeling</p>'; ?> 
- <?php echo "<font color='red'>Red</font>"; ?>
+ <?php echo "<font color='green'>Green</font>"; ?>
 <?php header("Refresh:0"); ?>
  </body>
 </html>
